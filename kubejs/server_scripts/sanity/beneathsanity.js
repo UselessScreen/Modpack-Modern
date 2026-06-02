c4e069eb07f98ae878bed7fd46d63d352ceba226
ThePheonix3k <101991744+ThePheonix3k@users.noreply.github.com>
2026-06-02 15:36:40 -0400

    added more mods (sanity)

---
+++ kubejs/server_scripts/sanity/beneathsanity.js
@@ -0,0 +1,17 @@
+// Passive sanity drain in the nether dimension
+PlayerEvents.tick(event => {
+    const { player } = event;
+
+    if (!(event.server.getTickCount() % 20)) { // Every second (20 ticks)
+        if (player.level.dimension == 'minecraft:the_nether') {
+            player.addSanity(-10);
+        }
+    }
+});
+
+// Separately, log sanity changes in chat
+SanityEvents.change(event => {
+    const { player } = event;
+    const newSanity = event.getSanity();
+    player.tell(`Sanity: ${newSanity}/100`);
+});
